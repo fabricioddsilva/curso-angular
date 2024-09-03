b20c59b2c3108489d3a2b2b13b967546673658e7
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-09-03 18:54:45 -0300

    Diretivas: ngIf

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrl: './diretiva-ngif.component.css'
+  styleUrl: './diretiva-ngif.component.scss'
 })
 export class DiretivaNgifComponent {
+  cursos: string[] = [];
 
+  mostrarCursos = false;
+
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 }
