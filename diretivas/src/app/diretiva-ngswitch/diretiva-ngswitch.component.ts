1a0804a06891ab4a30d04a8b1e1c6a8439b9ddfd
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-09-04 18:46:21 -0300

    ngSwitch, ngSwitchCase e ngSwitchDefault

---
+++ diretivas/src/app/diretiva-ngswitch/diretiva-ngswitch.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngswitch',
+  templateUrl: './diretiva-ngswitch.component.html',
+  styleUrl: './diretiva-ngswitch.component.scss'
+})
+export class DiretivaNgswitchComponent {
+
+  aba= 'home';
+}
